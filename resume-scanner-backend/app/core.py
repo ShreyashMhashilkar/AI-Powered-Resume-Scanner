fb3386258266f20ab56b66aff09f0df16b340cfa
ShreyashMhashilkar <itshreyash890@gmail.com>
2025-10-20 18:27:15 +0530

    new changes for vercel deployment

--- resume-scanner-backend/app/core.py
+++ resume-scanner-backend/app/core.py
@@ -1,12 +1,12 @@
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 def create_app():
     app = FastAPI(title="AI-Powered Resume Scanner")
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],  # Replace with your frontend URL in production
+        allow_origins=["*"],  # Replace with frontend URL in production
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
